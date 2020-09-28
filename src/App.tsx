2646f347bd1326c9e553dd5b0169ee7bd7db9a6f
Erik Fecher <efecher@gmail.com>
2020-09-28 15:47:17 -0400

    logic that determines if the questions on the current page are 'either/or' and which are disabled if you answer the other(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 ///<reference path="./typings/app.d.ts" />
 import React from 'react';
 import * as PageData from './questions/question-bank.json';
+import * as QuestionLogic from './questions/question-logic.json';
 import Page from './pages/page';
 import { Container, Row, Col } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { prop } from './util/util';
+import { CLIENT_RENEG_LIMIT } from 'tls';
 
 class App extends React.Component<AppProps, AppState> {
   constructor(props: AppProps) {
@@ -13,6 +15,7 @@ class App extends React.Component<AppProps, AppState> {
     this.state = {
       currentPage: 0,
       pages: PageData.pages,
+      questionLogic: QuestionLogic.logic,
       userInput: {} 
     } 
   } 
@@ -55,12 +58,57 @@ class App extends React.Component<AppProps, AppState> {
       }
     });
 
+    /* NOTE: determine if this is a situation like SAT/ACT where user 
+    * can answer the SAT questions, but then not ACT and vice-versa
+    * if so, then if the user enters a value for either SAT input, disable
+    * the ACT and vice-versa for the ACT
+    */
+
+    // NOTE: if there is a value in the input
+    if(parseInt(e.currentTarget.value) > 0) {
+      let _l: LogicOrPage = this.state.questionLogic[this.state.currentPage];
+      //console.log(_l);
+      for(let _q of _l.logicORLeft) {
+        if(_q === ssID) {
+          /* NOTE: if the current input is one of the left side, disable the right side
+          * of the 'or' 
+          */
+          for(let r of _l.logicORRight) {
+            //console.log(r.substring(5,(ssID.length - 5)));
+            (document.querySelector<HTMLInputElement>(`#${r.substring(5)}`))!.disabled = true;
+          }
+        }
+      }
+
+      for(let _q of _l.logicORRight) {
+        if(_q === ssID) {
+          /* NOTE: if the current input is one of the left side, disable the right side
+          * of the 'or' 
+          */
+          for(let r of _l.logicORLeft) {
+            //console.log(r);
+            (document.querySelector<HTMLInputElement>(`#${r.substring(5)}`))!.disabled = true;
+          }
+        }
+        break;
+      }
+    } else {
+      let _l: LogicOrPage = this.state.questionLogic[this.state.currentPage];
+      for(let _ssID of _l.logicORLeft) {
+        (document.querySelector<HTMLInputElement>(`#${_ssID.substring(5)}`))!.disabled = false;
+      }
+      for(let _ssID of _l.logicORRight) {
+        (document.querySelector<HTMLInputElement>(`#${_ssID.substring(5)}`))!.disabled = false;
+      }
+    }
     return; 
   }
 
   render = () => {
     // NOTE: make sure we're not on the last page
     if(this.state.currentPage < this.state.pages.length) {
+      let _ql: LogicOrPage = this.state.questionLogic[this.state.currentPage];
+      //console.log(this.state.questionLogic);
       // NOTE: Important to ensure the state keys are there
       // for the controlled inputs
       if(Object.keys(this.state.userInput).length > 0) {
